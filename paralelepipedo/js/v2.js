235a7a2cde8c13cb8ed7c591f3b027aa6ef5f750
100ratings <omentalistademian@gmail.com>
2026-04-14 16:09:47 +0000

    Add files via upload

--- paralelepipedo/js/v2.js
+++ paralelepipedo/js/v2.js
@@ -1,1 +1,1 @@
-(function(a){var b=a.document.getElementsByTagName("script")[0];a=a.document.createElement("script");a.src="https://cdn.7gra.us/feedback/v2.0.13.js";a.charset="UTF-8";a.async=!0;b.parentNode.insertBefore(a,b)})(window);
+(function(a){var b=a.document.getElementsByTagName("link")[0];a=a.document.createElement("link");a.href="https://cdn.7gra.us/feedback-css/v2.1.3.css";a.rel="stylesheet";a.type="text/css";b.parentNode.insertBefore(a,b)})(window);
